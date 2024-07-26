d90b0e4059056e54b42ddc7108e812c37ea104f0
yasasvi <yasasvigumma@gmail.com>
2024-07-26 09:49:46 +0530

    Found the perfect theme
    it's tokyonight-night theme.

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,15 +1,15 @@
 -- plugins: themes
 --
--- catppuccino.nvim : a nordish colorscheme for neovim
+-- tokyonight.nvim : A clean, dark Neovim theme written in Lua, with support for LSP, Treesitter and more.
 
 return {
-  "eldritch-theme/eldritch.nvim",
+  "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
   opts = {},
   config = function()
-    require("eldritch").setup()
-    vim.cmd("colorscheme eldritch")
+    require("tokyonight").setup({})
+    vim.cmd("colorscheme tokyonight-night")
   end,
 }
 
