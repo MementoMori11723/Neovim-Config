d7967acce7ce52efa4d4c390d1a4f1e7a043c19d
yasasvi <yasasvigumma@gmail.com>
2024-08-17 12:36:16 +0530

    Added a database viewer
    this will help us view our database.

--- lua/plugins/db-viewer.lua
+++ lua/plugins/db-viewer.lua
@@ -1,3 +1,12 @@
+-- plugin: vim-dadbod
+--
+-- This plugin is a database client for Vim. It allows you to connect to a database and run queries. It supports multiple databases, including SQLite, MySQL, PostgreSQL, and more.
+-- 
+
+
 return {
-  "tpope/vim-dadbod",
+  "tpope/vim-dadbod",                        -- Database access plugin for Vim
+  "kristijanhusak/vim-dadbod-ui",            -- UI for vim-dadbod
+  "kristijanhusak/vim-dadbod-completion",    -- Autocompletion for vim-dadbod
 }
+
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -115,11 +115,6 @@ return {
         },
       })
       vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {})
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        callback = function()
-          vim.lsp.buf.format({ async = true })
-        end,
-      })
     end,
   },
 }
