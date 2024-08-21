1a8e88361b39ab14cb6dcc774b6baff0402ed50e
yasasvi <yasasvigumma@gmail.com>
2024-08-21 11:38:23 +0530

    Removed looks
    didn't look good!

--- lua/plugins/syntax-tools.lua
+++ lua/plugins/syntax-tools.lua
@@ -128,11 +128,7 @@ return {
 	{
 		"lukas-reineke/indent-blankline.nvim",
 		main = "ibl",
-		opts = {
-      indent = {
-        char = '│',
-      },
-		},
+		opts = {},
 	},
   -- bufferline
   {
